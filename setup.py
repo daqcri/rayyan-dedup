c3e122a5278294f993db05452e26c93bc783154d
Hossam Hammady <github@hammady.net>
2017-03-06 15:49:24 +0300

    Fixing setup.py for correct packaging

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
 from setuptools import setup
 import sys
 
-requirements = ['future',
-                'dedupe>=1.2.3']
+requirements = ['future>=0.14',
+                'dedupe>=1.6',
+                'unidecode',
+                'psycopg2']
 
 if sys.version < '3':
     requirements += ['backports.csv']
 
 setup(
-    name = "csvdedupe",
-    version = '0.1.14',
-    description="Command line tools for deduplicating and merging csv files",
-    author="Forest Gregg, Derek Eder",
+    name = "rayyan-dedup",
+    version = '0.0.1',
+    description="csvdedupe-based command line tool for deduplicating Rayyan articles",
+    author="Hossam Hammady, Forest Gregg, Derek Eder",
     license="MIT",
     packages=['csvdedupe'],
     entry_points ={
         'console_scripts': [
-            'csvdedupe = csvdedupe.csvdedupe:launch_new_instance',
-            'csvlink = csvdedupe.csvlink:launch_new_instance'
+            'rayyan-dedup = csvdedupe.csvdedupe:launch_new_instance'
         ]
     },
     install_requires = requirements
